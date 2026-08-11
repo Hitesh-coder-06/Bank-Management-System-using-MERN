5f7853d0a1001afacf52b98c02c46730641765f0
Hitesh Rathore <hitesh8003864620@gmail.com>
2026-08-11 14:55:55 +0530

    Prepare project for deployment

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
-const app =require("./src/app")
 
-const connectToDb=require("./src/config/db")
+const app = require("./src/app");
+const connectToDb = require("./src/config/db");
 
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000,()=>{
-    console.log("server is running on port 3000")
-
-})
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`server is running on port ${PORT}`);
+});
